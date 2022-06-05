8ed1fc8866069cda042cf43eff8703222a6b214c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-05 15:51:01 +0900

    코틀린 반복문 (for) 체험

--- app/src/main/java/com/start/lottosimulator_20220605/MainActivity.kt
+++ app/src/main/java/com/start/lottosimulator_20220605/MainActivity.kt
@@ -2,6 +2,7 @@ package com.start.lottosimulator_20220605
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.start.lottosimulator_20220605.databinding.ActivityMainBinding
 
@@ -17,6 +18,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents(){
+
+        binding.btnBuyLotto.setOnClickListener {
+
+//            로또번호 6개 생성
+            makeLottoNumbers()
+
+//            보너스 번호 생성
+        }
+
+    }
+
+    fun makeLottoNumbers(){
+
+//        6개의 당첨 번호 => 반복횟수 명확 => for문
+        for(i in 0 until 6){
+
+            Log.d("숫자확인",i.toString())
+        }
 
     }
 
